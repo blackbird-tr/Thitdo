d52e5000fe145c54a4d403dccb8025f2a981a63a
Evren KARAKUŞ <109800322+blackbird-tr@users.noreply.github.com>
2024-07-03 15:53:56 +0300

    Initial Commit

---
+++ Thitdo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thitdo
+//
+//  Created by Evren Karakuş on 3.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thitdo/ThitdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThitdoApp.swift
+//  Thitdo
+//
+//  Created by Evren Karakuş on 3.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThitdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
